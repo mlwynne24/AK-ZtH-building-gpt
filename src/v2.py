af089c04a2cc56d102276887990a342eebf01d61
Morgan Wynne <mlwynne@btinternet.com>
2025-09-22 08:39:46 +0100

    immaterial removal of string ticks

--- src/v2.py
+++ src/v2.py
@@ -142,9 +142,7 @@ class GPT(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-            *[Block(n_embd, n_head=n_head) for _ in range(n_layer)]
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range (n_layer)])
         self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
